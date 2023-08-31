8783c3590650ebfceb03ebb0403ffe1f1ae69951
AwabIsam <awabcareers@gmail.com>
2023-08-31 17:19:25 +0400

    somechanges

--- app/page.js
+++ app/page.js
@@ -1,22 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import Image from "next/image";
-import Carusol from '@/components/carusol'
-import Hero from '@/components/Hero'
-import CredentialTestimonial from '@/components/CredentialTestimonial'
+import Carusol from "@/components/carusol";
+import HeroSlider from "@/components/HeroSlider";
+import CredentialTestimonial from "@/components/CredentialTestimonial";
 import "keen-slider/keen-slider.min.css";
 import Landing from "@/components/Landing";
 import AboutMe from "@/components/AboutMe";
 import Services from "@/components/Services";
 import Caruosel from "@/components/caruosel";
 export default function Home() {
-  return (
-    <>
-      <Landing/>
-      <Caruosel/>
-      <AboutMe/>
-      {/* <CredentialTestimonial/> */}
-      {/* <Services/> */}
-    </>
-  );
+	return (
+		<>
+			<Landing />
+			<Caruosel />
+			<AboutMe />
+			{/* <CredentialTestimonial/> */}
+			{/* <Services/> */}
+		</>
+	);
 }
--- components/Landing.js
+++ components/Landing.js
@@ -1,44 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 const Landing = () => {
-  return (
-    <div style={{height:"100vh", backgroundColor:"#0249a5",opacity:"0.5"}} className="block  flex-col content-evenly justify-evenly align-middle">
-      <Image
-        src={"/landing.jpeg"}
-        alt="landing"
-        fill
-        quality={100}
-        sizes="100vw 100vh"
-        style={{
-          objectFit: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          zIndex: "-1",
-          backgroundColor: "#0249A5",
-        }}
-      />
-      <div
-        // style={{ height: "10vh" }}
-        className=" flex flex-col .text-center justify-center align-middle text-center mt-4 "
-      >
-        <h3 className=".font-extrabold text-5xl text-white">
-          Al-Madrasatu al-Hidayah
-        </h3>
-      </div>
-      <div
-        height="110vh"
-        style={{ height: "50vh" }}
-        className="flex flex-col justify-end pb-9 align-middle text-center "
-      >
-        <h3 className="text-white font-bold text-4xl">
-          "Igniting Hearts with Quranic Light"
-        </h3>
-        <h4 className="text-white font-normal text-2xl">
-          Nurturing Muslim Children at the "al-Madrasatu al-Hidayah Quranic
-          School"
-        </h4>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={{ height: "90vh", backgroundColor: "#0249a5", opacity: "0.5" }}
+			className="block flex-col content-evenly justify-evenly align-middle"
+		>
+			<Image
+				src={"/landing.jpeg"}
+				alt="landing"
+				fill
+				quality={100}
+				sizes="100vw 100vh"
+				style={{
+					objectFit: "cover",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+					zIndex: "-1",
+					backgroundColor: "#0249A5",
+				}}
+			/>
+			<div
+				// style={{ height: "10vh" }}
+				className=" flex flex-col .text-center justify-center align-middle text-center mt-4 "
+			>
+				<h3 className=".font-extrabold text-5xl text-white">Al-Madrasatu al-Hidayah</h3>
+			</div>
+			<div height="110vh" style={{ height: "50vh" }} className="flex flex-col justify-end pb-9 align-middle text-center ">
+				<h3 className="text-white font-bold text-4xl">"Igniting Hearts with Quranic Light"</h3>
+				<h4 className="text-white font-normal text-2xl">Nurturing Muslim Children at the "al-Madrasatu al-Hidayah Quranic School"</h4>
+			</div>
+		</div>
+	);
 };
 export default Landing;
